0d3cb482dd128e8d86117f6f12dffc3f46665b75
Affan <mohammed.affan4@gmail.com>
2019-03-07 14:46:54 +0530

    First Setup

---
+++ src/App.js
@@ -0,0 +1,15 @@
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {}
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>h1</h1>
+      </div>
+    )
+  }
+}
+export default App
