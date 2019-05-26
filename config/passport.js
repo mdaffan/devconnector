e9c9b3500c7e4e8c621725dc23187f2d9cefb6ab
Affan <mohammed.affan4@gmail.com>
2019-05-26 20:42:13 +0530

    Finally

--- config/passport.js
+++ config/passport.js
@@ -1,14 +1,14 @@
-const jwtStrategy = require('passport-jwt').Strategy
-const extractJwt = require('passport-jwt').ExtractJwt
+const JwtStrategy = require('passport-jwt').Strategy
+const ExtractJwt = require('passport-jwt').ExtractJwt
 const mongoose = require('mongoose')
 const User = mongoose.model('users')
 const keys = require('../config/keys')
 const opts = {}
-opts.jwtFromRequest = extractJwt.fromAuthHeaderAsBearerToken()
-opts.secretOrKey = keys.secret
+opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken()
+opts.secretOrKey = keys.secretOrKey
 module.exports = passport => {
   passport.use(
-    new jwtStrategy(opts, (jwtPayload, done) => {
+    new JwtStrategy(opts, (jwtPayload, done) => {
       User.findById(jwtPayload.id)
         .then(user => {
           if (user) {
