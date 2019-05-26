02ced8c0a4daae257613a71ff94adf44d89105f0
Affan <mohammed.affan4@gmail.com>
2019-05-26 17:58:21 +0530

    still in confusion

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 const passport = require('passport')
 const app = express()
+const path = require('path')
 app.use(cors())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
@@ -26,8 +27,10 @@ app.use('/api/profile', profiles)
 app.use('/api/posts', posts)
 //Serve Static Assests if in production
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static())
+  app.use(express.static('client/public'))
 }
-
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'client', 'public', 'index.html'))
+})
 const port = process.env.PORT || 5000
 app.listen(port, () => console.log(`Server running on port ${port}`))
